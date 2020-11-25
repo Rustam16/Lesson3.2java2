9f1290bebb6f10de941d18c82b1fdaea1ae137ab
Rustam16 <muhambetovrustam1@gmail.com>
2020-11-25 22:37:19 +0600

    исправление.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,20 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(20000);
-        Scanner sc= new Scanner(System.in);
             while (true) {
             System.out.println(bankAccount.getAmount());
             try {
                 bankAccount.withDraw(6000);
-                sc.nextInt();
             } catch (LimitException a) {
-                System.out.println(a.getMessage()+sc.nextInt());
+                System.out.println(a.getMessage());
                 try {
-                    sc.nextInt();
                     bankAccount.withDraw((int) (a.getRemainingAmount()));
                 } catch (LimitException e) {
                     System.out.println(e.getMessage());
-                    System.out.println(sc.nextLine());
                 }
                 break;
             }
